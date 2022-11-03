d7bccca162de30daee8429fac5848779f0f5570a
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-03 11:22:18 -0300

    Categorias dinamicas

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,11 @@
+// Libraries
+import { Link } from "react-router-dom";
+
 // Components
 import CartWidget from "./CartWidget";
+
+// Data
+import { categories } from "../util/categoriesData";
 
 // Icons
 import { BsFileEarmarkCode } from "react-icons/bs";
@@ -8,10 +14,12 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
       <div className="container-fluid">
-        <a href="#" className="navbar-brand d-flex align-items-center gap-2">
-          <BsFileEarmarkCode className="fs-2"/>
-          <span style={{color: "#20c997"}} className="font-monospace">LOREM Shop</span>
-        </a>
+        <Link to="/" className="navbar-brand d-flex align-items-center gap-2">
+          <BsFileEarmarkCode className="fs-2" />
+          <span style={{ color: "#20c997" }} className="font-monospace">
+            LOREM Shop
+          </span>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -26,25 +34,30 @@ const NavBar = () => {
 
         <div className="collapse navbar-collapse" id="menu">
           <ul className="navbar-nav mx-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Categoría 0</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Categoría 1</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Categoría 2</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Categoría 3</a>
-            </li>
+            {Object.keys(categories).map((category, index) => {
+              return (
+                <li className="nav-item" key={index}>
+                  <Link
+                    className="nav-link"
+                    to={`/category/${categories[category]}`}
+                  >
+                    {category}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
-          <hr className="text-white-50"/>
+          <hr className="text-white-50" />
           <div className="d-flex gap-3 align-items-center">
-            <button type="button" className="btn btn-outline-primary px-5 login-button">Login</button>
+            <button
+              type="button"
+              className="btn btn-outline-primary px-5 login-button"
+            >
+              Login
+            </button>
             <CartWidget />
           </div>
-          <hr className="text-white-50"/>
+          <hr className="text-white-50" />
         </div>
       </div>
     </nav>
