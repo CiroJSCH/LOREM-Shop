f75e4d24c5ebe5d2d058dd355e15032b37c775b3
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-03 11:22:40 -0300

    Estilo en linea

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link, Navigate } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ItemListContainer = ({ greeting }) => {
   const [products, setProducts] = useState([]);
+  const { category_id } = useParams();
 
   useEffect(() => {
     axios
       .get("https://62ef11618d7bc7c2eb74befd.mockapi.io/products")
       .then((response) => {
-        // console.log(response.data)
-        setProducts(response.data);
+        category_id
+          ? setProducts(
+              response.data.filter(
+                (product) => product.category_id === category_id
+              )
+            )
+          : setProducts(response.data);
       })
       .catch((error) => console.log(error));
-  }, []);
+  }, [category_id]);
 
   return (
     <div className="container d-flex justify-content-center align-items-center mt-5">
@@ -22,16 +28,18 @@ const ItemListContainer = ({ greeting }) => {
           return (
             <div className="col-12 col-md-6 col-lg-4 col-xl-3" key={index}>
               <div className="card mb-3">
-                <img className="card-img-top" src={product.image} alt={`${product.name} image`}></img>
+                <img
+                  className="card-img-top"
+                  src={product.image}
+                  alt={`${product.name} image`}
+                  style={{"height":"21rem"}}
+                ></img>
                 <div className="card-body bg-dark rounded-bottom border-bottom border-primary border-5">
                   <h5 className="card-title mb-3">{product.name}</h5>
                   <p className="card-text mb-4">
-                    {product.description.substring(0,70)}
+                    {product.description.substring(0, 70)}
                   </p>
-                  <Link
-                    to={`/item/${product.id}`}
-                    className="btn btn-primary"
-                  >
+                  <Link to={`/item/${product.id}`} className="btn btn-primary">
                     View Detail
                   </Link>
                 </div>
