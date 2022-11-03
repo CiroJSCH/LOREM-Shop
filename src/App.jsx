4c22691b92abd8d37a4d4bcecae0e0cf2f5c2cba
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-03 11:22:54 -0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
         <NavBar />
         <Routes>
           <Route path="/" element={<ItemListContainer />} />
-          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/category/:category_id" element={<ItemListContainer />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
         </Routes>
       </BrowserRouter>
