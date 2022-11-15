ada5807f1ca90c2bb6a391acee34d82d8cb62404
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-15 11:51:35 -0300

    Funcion para calcular cantidad de items movida

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -11,18 +11,13 @@ import { RiShoppingCart2Line } from "react-icons/ri";
 const CartWidget = () => {
 
   const { cartList } = useContext(CartContext);
-
-  let items = 0;
-
-  cartList.map(product => {
-    items += product.quantity;
-  })
+  const { calcItemsQuantity } = useContext(CartContext);
 
   return (
     <Link to="/cart" className="position-relative">
       <RiShoppingCart2Line className="fs-2 text-primary"/>
       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-        {items}
+        {calcItemsQuantity(cartList)}
       </span>
     </Link>
   );
