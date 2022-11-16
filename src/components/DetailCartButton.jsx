ef9ad2cb3af299230c8e43f1860afc9c2532127e
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-15 22:38:56 -0300

    Evitar duplicados cart

--- src/components/DetailCartButton.jsx
+++ src/components/DetailCartButton.jsx
@@ -1,5 +1,6 @@
 /// Hooks
 import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 
 // Context
 import { CartContext } from "../context/CartContext";
@@ -10,6 +11,7 @@ import { RiShoppingCart2Line } from "react-icons/ri";
 
 const DetailCartButton = ({ item }) => {
   const [items, setItems] = useState(0);
+  const [inCart, setInCart] = useState(false);
 
   const removeItem = () => {
     items > 0 ? setItems(items - 1) : setItems(0);
@@ -19,33 +21,54 @@ const DetailCartButton = ({ item }) => {
     items < item.stock ? setItems(items + 1) : setItems(item.stock);
   };
 
-  const { addToCart }= useContext(CartContext)
+  const { addToCart } = useContext(CartContext);
 
   return (
-    <div className="d-flex flex-wrap gap-4 mb-2">
-      <div className="buttons">
-        <button className="buttons-change button-minus" onClick={removeItem}>
-          <CgMathMinus />
-        </button>
-        <span className="buttons-counter">{items}</span>
-        <button className="buttons-change button-plus" onClick={addItem}>
-          <CgMathPlus />
-        </button>
-      </div>
-      <button className="d-flex align-items-center justify-content-center gap-2 checkout" disabled={items === 0} onClick={() => addToCart(item, items)}>
-        <RiShoppingCart2Line /> Add to cart
-      </button>
-      <span
-        className="badge bg-secondary fs-4 d-flex align-items-center justify-content-center"
-        style={{
-          fontFamily: "Titillium Web, sans-serif",
-          minHeight: "3rem",
-          minWidth: "9rem",
-        }}
-      >
-        Stock: {item.stock}
-      </span>
-    </div>
+    <>
+      {!inCart ? (
+        <div className="d-flex flex-wrap gap-4 mb-2">
+          <div className="buttons">
+            <button
+              className="buttons-change button-minus"
+              onClick={removeItem}
+            >
+              <CgMathMinus />
+            </button>
+            <span className="buttons-counter">{items}</span>
+            <button className="buttons-change button-plus" onClick={addItem}>
+              <CgMathPlus />
+            </button>
+          </div>
+          <button
+            className="d-flex align-items-center justify-content-center gap-2 checkout"
+            disabled={items === 0}
+            onClick={() => {
+              setInCart(true);
+              addToCart(item, items);
+            }}
+          >
+            <RiShoppingCart2Line /> Add to cart
+          </button>
+          <span
+            className="badge bg-secondary fs-4 d-flex align-items-center justify-content-center"
+            style={{
+              fontFamily: "Titillium Web, sans-serif",
+              minHeight: "3rem",
+              minWidth: "9rem",
+            }}
+          >
+            Stock: {item.stock}
+          </span>
+        </div>
+      ) : (
+        <Link
+          to="/cart"
+          className="d-flex align-items-center justify-content-center gap-2 text-decoration-none checkout"
+        >
+          <RiShoppingCart2Line /> Checkout
+        </Link>
+      )}
+    </>
   );
 };
 
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,23 +1,53 @@
 // Libraries
 import { createContext, useState, useEffect } from "react";
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
-const MySwal = withReactContent(Swal)
+const MySwal = withReactContent(Swal);
 
 export const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
   const [cartList, setCartList] = useState([]);
-  
+
   useEffect(() => {
-    localStorage.getItem('cart') ? setCartList(JSON.parse(localStorage.getItem('cart'))) : setCartList([])
-  }, [])
-  
+    localStorage.getItem("cart")
+      ? setCartList(JSON.parse(localStorage.getItem("cart")))
+      : setCartList([]);
+  }, []);
+
   const addToCart = (product, quantity) => {
-    product["quantity"] = quantity;
-    localStorage.setItem('cart', JSON.stringify([...cartList, product]));
-    setCartList([...cartList, product]);
+    let tempCart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    let inCart = tempCart.find((item) => {
+      return item.id === product.id;
+    });
+
+    if (!inCart) {
+      product["quantity"] = quantity;
+      tempCart.push(product);
+      localStorage.setItem("cart", JSON.stringify(tempCart));
+      setCartList(tempCart);
+    } else {
+      console.log(tempCart);
+      let actualCart = tempCart.map((item, index) => {
+        if (item.id === product.id) {
+          // parseInt(tempCart[index]["quantity"]) + quantity >= tempCart[index]["stock"]
+          //   ? (tempCart[index]["quantity"] = parseInt(tempCart[index]["stock"]))
+          //   : (tempCart[index]["quantity"] =
+          //       parseInt(tempCart[index]["quantity"]) + quantity);
+          tempCart[index]["quantity"] = parseInt(tempCart[index]["quantity"]) + quantity;
+        }
+        return tempCart[index];
+      });
+      console.log(actualCart);
+      localStorage.setItem("cart", JSON.stringify(actualCart));
+      setCartList(actualCart);
+    }
+
+    // localStorage.setItem("cart", JSON.stringify([...cartList, product]));
+    // setCartList([...cartList, product]);
+    // console.log(inCart)
   };
 
   const removeOfCart = (product) => {
@@ -26,14 +56,17 @@ const CartContextProvider = ({ children }) => {
       showDenyButton: true,
       confirmButtonText: <p className="swalert">Yes</p>,
       denyButtonText: <p className="swalert">Cancel</p>,
-      confirmButtonColor: '#20c997',
-      denyButtonColor: '#d33',
+      confirmButtonColor: "#20c997",
+      denyButtonColor: "#d33",
     }).then((result) => {
       if (result.isConfirmed) {
-        localStorage.setItem('cart', JSON.stringify(cartList.filter((item) => item.id !== product.id)));
+        localStorage.setItem(
+          "cart",
+          JSON.stringify(cartList.filter((item) => item.id !== product.id))
+        );
         setCartList(cartList.filter((item) => item.id !== product.id));
       }
-    })
+    });
   };
 
   const calcItemsQuantity = (cartList) => {
