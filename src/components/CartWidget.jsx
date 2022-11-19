a76af4460aa118aa80e0498e766827034d62fbb1
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-19 11:26:18 -0300

    SCR notificacion productos

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -9,16 +9,17 @@ import { CartContext } from "../context/CartContext";
 import { RiShoppingCart2Line } from "react-icons/ri";
 
 const CartWidget = () => {
-
   const { cartList } = useContext(CartContext);
   const { calcItemsQuantity } = useContext(CartContext);
 
   return (
     <Link to="/cart" className="position-relative">
-      <RiShoppingCart2Line className="fs-2 text-primary"/>
-      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-        {calcItemsQuantity(cartList)}
-      </span>
+      <RiShoppingCart2Line className="fs-2 text-primary" />
+      {cartList.length > 0 && (
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          {calcItemsQuantity(cartList)}
+        </span>
+      )}
     </Link>
   );
 };
