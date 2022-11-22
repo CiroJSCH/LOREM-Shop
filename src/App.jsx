a6fab0144b74680827e72fa62ac0cfc3f3ae2ca7
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-21 21:44:26 -0300

    Todo LIST actualizada

--- src/App.jsx
+++ src/App.jsx
@@ -36,5 +36,6 @@ export default App;
 //* TODO: Slider productos relacionados en ItemDetail
 //* TODO: Banner página principal
 //* TODO: Arreglar lo del id
-// TODO: Orden de compra en Cart (Sacar la tabla y ver donde poner el boton para eliminar)
+//* TODO: Orden de compra en Cart (Sacar la tabla y ver donde poner el boton para eliminar)
 //* TODO: Footer
+// TODO: Completar esquema de validación
