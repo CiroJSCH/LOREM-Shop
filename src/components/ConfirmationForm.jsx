96837d707bf4fe210ee73e8b352214f26b20a4d8
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-23 18:39:55 -0300

    Limpiar carrito y restar stock de cada item

--- src/components/ConfirmationForm.jsx
+++ src/components/ConfirmationForm.jsx
@@ -1,6 +1,14 @@
 // Libraries
+import { useContext } from "react";
 import { Button } from "@mui/material";
 import { Field, Form, Formik, ErrorMessage } from "formik";
+import { serverTimestamp, collection, doc, setDoc, updateDoc, increment } from "firebase/firestore";
+
+// Context
+import { CartContext } from "../context/CartContext";
+
+// Firebase
+import { db } from "../firebase/firebaseConfig";
 
 // Util
 import initialValues from "../util/initialValues";
@@ -8,14 +16,54 @@ import { validationSchema } from "../util/validationSchema";
 import CustomTextField from "../util/customTextField";
 
 const ConfirmationForm = ({ setShowForm }) => {
+  const { cartList, calcTotal, emptyCar } = useContext(CartContext);
+
+  const createOrder = (values, formikHelpers) => {
+    let order = {
+      buyer: {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      },
+      date: serverTimestamp(),
+      items: cartList.map((item) => ({
+        id: item.id,
+        price: item.price,
+        title: item.name,
+        qty: item.quantity,
+      })),
+      total: calcTotal(),
+    };
+
+    const orderInFirestore = async() => {
+      const newOrderRef = doc(collection(db, 'orders'));
+      await setDoc(newOrderRef, order);
+      return newOrderRef;
+    }
+
+    orderInFirestore()
+      .then(response => {
+        cartList.map(async (item) => {
+          const itemRef = doc(db, 'products', item.id);
+          await updateDoc(itemRef, {
+            stock: increment(-item.quantity)
+          });
+        })
+        emptyCar();
+      })
+      .catch(error => console.log(error));
+
+    formikHelpers.resetForm();
+  };
+
+
   return (
     <div>
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={(values, formikHelpers) => {
-          console.log(values);
-          formikHelpers.resetForm();
+          createOrder(values, formikHelpers);
         }}
       >
         {({ errors, touched }) => (
