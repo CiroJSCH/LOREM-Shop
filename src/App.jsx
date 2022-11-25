fcd16100ae8f871f9f3ef83609a9ce5a673dc6f3
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-11-25 11:44:02 -0300

    Todo List eliminada

--- src/App.jsx
+++ src/App.jsx
@@ -27,19 +27,3 @@ const App = () => {
 };
 
 export default App;
-
-
-//* TODO: LocalStorage carrito
-//* TODO: Remover productos del carrito (Con alerta)
-//* TODO: Quitar zona de añadir al carrito cuando ya añadi un item
-//* TODO: Validar que no se pase del stock
-//* TODO: Slider productos relacionados en ItemDetail
-//* TODO: Banner página principal
-//* TODO: Arreglar lo del id
-//* TODO: Orden de compra en Cart (Sacar la tabla y ver donde poner el boton para eliminar)
-//* TODO: Footer
-//* TODO: Completar esquema de validación
-//* TODO: Generar orden de compra con React PDF
-//* TODO: Readme
-// TODO: Revisar código
-// TODO: Hacer Deploy
